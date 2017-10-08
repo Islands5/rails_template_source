066e153358bbae8b33a9b78a70b66cb9132308e4
Ryotaro Goto <five.islands.04010613@gmail.com>
2017-10-08 16:30:41 +0900

    修正

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-bundle install --path vendor/bundler
+bundle install --path vendor/bundle
 rm -f /%app_name/tmp/pids/server.pid
 
 if [ "$DEBUG_MODE" = "1" ] ; then
-  bundle exec rdebug-ide --host 127.0.0.1 --port 1234 --dispatcher-port 26162 -- bin/rails s -b 0.0.0.0 -p 3000 -e development
+  bundle exec rdebug-ide --host 0.0.0.0 --port 1234 --dispatcher-port 26162 -- bin/rails s -b 0.0.0.0 -p 3000 -e development
 else
-  bin/rails s -p 3000 -b '127.0.0.1' -e development
+  bin/rails s -p 3000 -b '0.0.0.0' -e development
 fi
