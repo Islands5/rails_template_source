66e5f8210a1035dd2cb553a150b22eecc35e04ff
Ryotaro Goto <five.islands.04010613@gmail.com>
2017-10-09 01:00:44 +0900

    worddirがアプリケーションのルートだから変更

--- shell_scripts/start.sh
+++ shell_scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 bundle install --path vendor/bundle
-rm -f /%app_name%/tmp/pids/server.pid
+rm -f tmp/pids/server.pid
 
 if [ "$DEBUG_MODE" = "1" ] ; then
   bundle exec rdebug-ide --host 0.0.0.0 --port 1234 --dispatcher-port 26162 -- bin/rails s -b 0.0.0.0 -p 3000 -e development
