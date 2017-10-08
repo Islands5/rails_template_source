0c6732294787d6ca130afa5b8d5737cee11d4a49
Ryotaro Goto <five.islands.04010613@gmail.com>
2017-10-08 11:08:04 +0900

    テンプレート修正

---
+++ template.rb
@@ -0,0 +1,26 @@
+# view
+gem 'slim-rails'
+gem 'sass-rails'
+
+# env
+gem 'dotenv-rails'
+
+gem 'devise'
+# gem 'authlogic'
+
+# gem 'pundit
+
+# gem 'redis'
+# gem 'sidekiq'
+
+gem_group :development do
+  gem 'pry-byebug'
+  gem 'pry-doc'
+  gem 'bullet'
+end
+
+gem_group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'rubocop', require: false
+end
